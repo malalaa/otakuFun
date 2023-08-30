00d59a5e05fa273d5dc85d96bcb2ac7a4ddc3680
malala <1482775872@qq.com>
2023-08-30 10:15:25 +0800

    modify otakuFun2

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -3,7 +3,7 @@ import multiprocessing
 bind = "127.0.0.1:5000"
 workers = multiprocessing.cpu_count() * 2 + 1
 threads = 10
-accesslog = "/var/log/otakuFun2/gunicorn_access.log"
-errorlogb = "/var/log/otakuFun2/gunicorn_error.log"
+accesslog = "/var/log/otakuFun/gunicorn_access.log"
+errorlogb = "/var/log/otakuFun/gunicorn_error.log"
 preload_app = True
 daemon = True
